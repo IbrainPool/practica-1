958d8cd33adb11b1eb9c72adb6508cb461ab43c6
Ibrain <82613820+IbrainPool@users.noreply.github.com>
2021-04-26 12:55:06 -0500

    creacion de modelos

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 'use strict'
+
+var mongoose = require('mongoose')
+var port = process.env.port || 3789;
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/Tienda', {useNewUrlParser: true})
+    .then(() => {
+        console.log('La conexion de base de datos se ha realizado bien');
+    })
+    .catch(err =>{
+        console.error('Error de conexión');
+        console.error(err);
+    });
+
